e6f0065fb29c05aea49ee019daeeef3eefeabdb1
wangyuxi <yuxi007008@163.com>
2019-05-02 01:58:47 +0800

    bug fix and add jedis cluster support

--- src/main/java/org/codinjutsu/tools/nosql/redis/logic/RedisClient.java
+++ src/main/java/org/codinjutsu/tools/nosql/redis/logic/RedisClient.java
@@ -17,11 +17,9 @@
 package org.codinjutsu.tools.nosql.redis.logic;
 
 import com.google.common.collect.Maps;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
 import com.intellij.util.ArrayUtil;
 import kotlin.text.Charsets;
 import org.apache.commons.lang.StringUtils;
@@ -31,12 +29,10 @@ import org.codinjutsu.tools.nosql.commons.logic.DatabaseClient;
 import org.codinjutsu.tools.nosql.commons.model.AuthenticationSettings;
 import org.codinjutsu.tools.nosql.commons.model.Database;
 import org.codinjutsu.tools.nosql.commons.model.DatabaseServer;
-import org.codinjutsu.tools.nosql.redis.model.RedisDatabase;
-import org.codinjutsu.tools.nosql.redis.model.RedisKeyType;
-import org.codinjutsu.tools.nosql.redis.model.RedisQuery;
-import org.codinjutsu.tools.nosql.redis.model.RedisResult;
+import org.codinjutsu.tools.nosql.redis.model.*;
 import redis.clients.jedis.*;
 
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -120,49 +116,103 @@ public class RedisClient implements DatabaseClient {
 
 
     public RedisResult loadRecords(ServerConfiguration serverConfiguration, RedisDatabase database, RedisQuery query, RedisQueryExecutor executor) {
-        RedisResult redisResult = new RedisResult();
-        if (createJedis(serverConfiguration) instanceof Jedis) {
-            Jedis jedis = (Jedis) createJedis(serverConfiguration);
+        JedisCommands commands = createJedis(serverConfiguration);
+        byte[] filterCondition = query.getFilter().getBytes(Charsets.UTF_8);
+        if (commands instanceof Jedis) {
+            Jedis jedis = (Jedis) commands;
             jedis.connect();
-            int index = Integer.parseInt(database.getName());
-            jedis.select(index);
-
+            jedis.select(Integer.parseInt(database.getName()));
             executor.handleRedisQuery(jedis);
+            return keySearch(jedis, filterCondition);
+        }
+        if (commands instanceof JedisCluster) {
+            JedisCluster cluster = (JedisCluster) commands;
+            executor.handleRedisQuery(commands);
+            return keySearch(cluster, filterCondition);
+        }
+        return null;
+    }
 
-            Set<byte[]> keys = jedis.keys(query.getFilter().getBytes(Charsets.UTF_8));
-            for (byte[] key : keys) {
-                //may be null pointer.
-                String type = jedis.type(key);
-                RedisKeyType keyType = RedisKeyType.getKeyType(type);
+    private RedisResult keySearch(JedisCommands commands, byte[] filterCondition) {
+
+        if (commands instanceof Jedis) {
+            Jedis jedis = (Jedis) commands;
+            return sortKeys(jedis, jedis.keys(filterCondition));
+        }
+        if (commands instanceof JedisCluster) {
+            JedisCluster cluster = (JedisCluster) commands;
+            TreeSet<RedisResult> resultTreeSet = new TreeSet<>();
+            Map<String, JedisPool> clusterNodes = cluster.getClusterNodes();
+            clusterNodes.keySet().stream().map(clusterNodes::get).map(JedisPool::getResource).forEachOrdered(conn -> {
+                try {
+                    resultTreeSet.add(sortKeys(conn, conn.keys(filterCondition)));
+                } catch (Exception e) {
+                } finally {
+                    conn.close();
+                }
+            });
+
+            RedisResult result = new RedisResult();
+            final List<RedisRecord> redisRecords = new LinkedList<>();
+            resultTreeSet.forEach(x -> redisRecords.addAll(x.getResults()));
+            redisRecords.forEach(redisRecord -> {
+                RedisKeyType keyType = redisRecord.getKeyType();
+                String key = redisRecord.getKey();
+                byte[] keyBytes = key.getBytes(StandardCharsets.UTF_8);
+                Object value = redisRecord.getValue();
                 if (RedisKeyType.LIST.equals(keyType)) {
-                    List<byte[]> values = jedis.lrange(key, 0, -1);
-                    List<String> collect = values.stream().map(b -> convertByteToString(b)).collect(Collectors.toList());
-                    redisResult.addList(convertByteToString(key), collect,key);
+                    result.addList(key, (List) value, keyBytes);
                 } else if (RedisKeyType.SET.equals(keyType)) {
-                    Set<byte[]> values = jedis.smembers(key);
-                    Set<String> collect = values.stream().map(b -> convertByteToString(b)).collect(Collectors.toSet());
-                    redisResult.addSet(convertByteToString(key), collect,key);
+                    result.addSet(key, (Set) value, keyBytes);
                 } else if (RedisKeyType.HASH.equals(keyType)) {
-                    Map<byte[], byte[]> values = jedis.hgetAll(key);
-                    Map<String, String> myValues = Maps.newHashMap();
-                    for (byte[] bytes : values.keySet()) {
-                        myValues.put(convertByteToString(bytes), convertByteToString(values.get(bytes)));
-                    }
-                    redisResult.addHash(convertByteToString(key), myValues,key);
+                    result.addHash(key, (Map) value, keyBytes);
                 } else if (RedisKeyType.ZSET.equals(keyType)) {
-                    Set<Tuple> valuesWithScores = jedis.zrangeByScoreWithScores(key, "-inf".getBytes(Charsets.UTF_8), "+inf".getBytes(Charsets.UTF_8));
-                    redisResult.addSortedSet(convertByteToString(key), valuesWithScores,key);
+                    result.addSortedSet(key, (Set<Tuple>) value, keyBytes);
                 } else if (RedisKeyType.STRING.equals(keyType)) {
-                    byte[] value = jedis.get(key);
-                    redisResult.addString(convertByteToString(key), convertByteToString(value),key);
+                    result.addString(key, (String) value, keyBytes);
                 } else {
                     //ignore this.
-                    throw new RuntimeException("unSupport type:" + type);
+                    throw new RuntimeException("unSupport type:" + keyType);
                 }
-            }
-        } else if (createJedis(serverConfiguration) instanceof JedisCluster) {
-            JedisCluster jedisCluster = (JedisCluster) createJedis(serverConfiguration);
 
+            });
+            return result;
+        }
+
+        return null;
+    }
+
+    private RedisResult sortKeys(Jedis jedis, Set<byte[]> keys) {
+        RedisResult redisResult = new RedisResult();
+        for (byte[] key : keys) {
+            //may be null pointer.
+            String type = jedis.type(key);
+            RedisKeyType keyType = RedisKeyType.getKeyType(type);
+            if (RedisKeyType.LIST.equals(keyType)) {
+                List<byte[]> values = jedis.lrange(key, 0, -1);
+                List<String> collect = values.stream().map(this::convertByteToString).collect(Collectors.toList());
+                redisResult.addList(convertByteToString(key), collect, key);
+            } else if (RedisKeyType.SET.equals(keyType)) {
+                Set<byte[]> values = jedis.smembers(key);
+                Set<String> collect = values.stream().map(this::convertByteToString).collect(Collectors.toSet());
+                redisResult.addSet(convertByteToString(key), collect, key);
+            } else if (RedisKeyType.HASH.equals(keyType)) {
+                Map<byte[], byte[]> values = jedis.hgetAll(key);
+                Map<String, String> myValues = Maps.newHashMap();
+                for (byte[] bytes : values.keySet()) {
+                    myValues.put(convertByteToString(bytes), convertByteToString(values.get(bytes)));
+                }
+                redisResult.addHash(convertByteToString(key), myValues, key);
+            } else if (RedisKeyType.ZSET.equals(keyType)) {
+                Set<Tuple> valuesWithScores = jedis.zrangeByScoreWithScores(key, "-inf".getBytes(Charsets.UTF_8), "+inf".getBytes(Charsets.UTF_8));
+                redisResult.addSortedSet(convertByteToString(key), valuesWithScores, key);
+            } else if (RedisKeyType.STRING.equals(keyType)) {
+                byte[] value = jedis.get(key);
+                redisResult.addString(convertByteToString(key), convertByteToString(value), key);
+            } else {
+                //ignore this.
+                throw new RuntimeException("unSupport type:" + type);
+            }
         }
         return redisResult;
     }
@@ -188,19 +238,20 @@ public class RedisClient implements DatabaseClient {
             }
             if (servers.length == 1) {
                 String redisUri = "redis://";
-                String redisPwd = serverConfiguration.getAuthenticationSettings().getPassword();
-                if (StringUtils.isNotEmpty(redisPwd)) {
-                    redisUri += ":" + redisPwd + "@";
-                }
+                String password = serverConfiguration.getAuthenticationSettings().getPassword();
+//    In this way, when special characters are input(example: characters '[' or ']'), special characters will be abnormal.
+
+//                if (StringUtils.isNotEmpty(password)) {
+//                    redisUri += ":" + password + "@";
+//                }
                 redisUri += serverConfiguration.getServerUrl();
                 Jedis jedis = new Jedis(redisUri);
-                String password = serverConfiguration.getAuthenticationSettings().getPassword();
                 if (StringUtils.isNotEmpty(password)) {
                     jedis.auth(password);
                 }
                 return jedis;
             }
         }
-        return null;
+        throw new RuntimeException("Service configuration error:");
     }
 }
